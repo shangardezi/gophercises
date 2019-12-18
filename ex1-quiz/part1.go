be6527946e79d554802c514a9ef6551ca28db53f
Shan Gardezi <shangardezi@gmail.com>
2019-12-18 21:06:28 +0000

    Part 1: Basic CSV parsing and input comparison

--- ex1-quiz/part1.go
+++ ex1-quiz/part1.go
@@ -1,62 +1,45 @@
 package main
 
 import (
+	"bufio"
 	"encoding/csv"
-	"flag"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
 	"os"
 	"strings"
 )
 
 func main() {
-	var fileName = flag.String("filename", "problems.csv", "Name of CSV file")
-	flag.Parse()
-
-	csvFile, err := os.Open(*fileName)
+	data, err := ioutil.ReadFile("./problems.csv")
 	if err != nil {
-		exit(fmt.Sprintf("Failed to open file with name: %s\n", *fileName))
+		panic(err)
 	}
 
-	reader := csv.NewReader(csvFile)
-	lines, err := reader.ReadAll()
-	if err != nil {
-		exit("Failed to parse the CSV file.")
-	}
+	r := csv.NewReader(strings.NewReader(string(data)))
+	reader := bufio.NewReader(os.Stdin)
+	score := 0
+	totalQuestions := 0
 
-	problems := parseLines(lines)
-	var correctAnswerCount = 0
-
-	for i, p := range problems {
-		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
-		var answer string
-		fmt.Scanf("%s\n", &answer)
-		if answer == p.a {
-			correctAnswerCount++
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
 		}
-	}
-
-	msg := "Your Score: %d / %d"
-	result := fmt.Sprintf(msg, correctAnswerCount, len(problems))
-	fmt.Println(result)
-}
-
-func parseLines(lines [][]string) []problem {
-	problems := make([]problem, len(lines))
-	for i, line := range lines {
-		problems[i] = problem {
-			q: line[0],
-			a: strings.TrimSpace(line[1]),
+		if err != nil {
+			log.Fatal(err)
 		}
+
+		fmt.Println(record[0])
+		text, _ := reader.ReadString('\n')
+
+		if strings.TrimRight(text, "\n") == record[1] {
+			score++
+		}
+
+		totalQuestions++
 	}
-	return problems
-}
 
-type problem struct {
-	q string
-	a string
-}
-
-func exit(msg string) {
-	fmt.Println(msg)
-	os.Exit(1)
+	fmt.Println("You scored:", score, "/", totalQuestions)
 }

c533c047dd1e4433841cc1308297190254555092
Shan Gardezi <shangardezi@gmail.com>
2019-12-19 16:35:01 +0000

    Add timer using channels

--- ex1-quiz/part1.go
+++ ex1-quiz/part1.go
@@ -5,63 +5,85 @@ import (
 	"encoding/csv"
 	"flag"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
 	csvFilename := flag.String("filename", "problems.csv", "The CSV file's name in the format of 'question,answer'")
+	timeLimit := flag.Int("timelimit", 30, "the duration of the quiz in seconds")
+
 	flag.Parse()
 
-	data, err := ioutil.ReadFile(*csvFilename)
+	file, err := os.Open(*csvFilename)
 
 	if err != nil {
-		error(fmt.Sprintf("Unable to open file named: %s", *csvFilename))
+		exit(fmt.Sprintf("Unable to open file named: %s", *csvFilename))
 	}
 
-	r := csv.NewReader(strings.NewReader(string(data)))
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit("Unable to read CSV file")
+	}
+
 	fmt.Println("Press enter to start the quiz")
 
 	buf := bufio.NewReader(os.Stdin)
 	_, err2 := buf.ReadBytes('\n')
 
 	if err2 != nil {
-        error("Unable to start quiz")
-	} else {
-		quiz(r)
+		exit("Unable to start quiz")
+	}
+
+	problems := parseLines(lines)
+	score := 0
+
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+
+	for i, p := range problems {
+		fmt.Printf("Problem %d of %d: %s\n", i+1, len(problems), p.q)
+		answerCh := make(chan string)
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerCh <- answer
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Printf("Time's up! You scored: %d/%d\n", score, len(problems))
+			return
+		case answer := <- answerCh:
+			if answer == p.a {
+				score++
+			}
+		}
+
+		fmt.Printf("\nYou scored: %d/%d\n", score, len(problems))
 	}
 }
 
-func error(msg string) {
+type problem struct {
+	q string
+	a string
+}
+
+func exit(msg string) {
 	fmt.Println(msg)
 	os.Exit(1)
 }
 
-func quiz(r *csv.Reader) {
-	reader := bufio.NewReader(os.Stdin)
-	score := 0
-	totalQuestions := 0
+func parseLines(lines [][]string) []problem {
+	problems := make([]problem, len(lines))
 
-	for {
-		record, err := r.Read()
-		if err == io.EOF {
-			break
+	for i, line := range lines {
+		problems[i] = problem{
+			q: line[0],
+			a: strings.TrimSpace(line[1]),
 		}
-		if err != nil {
-            error("Error reading file data")
-		}
-
-		fmt.Println(record[0])
-		text, _ := reader.ReadString('\n')
-
-		if strings.TrimRight(text, "\n") == record[1] {
-			score++
-		}
-
-		totalQuestions++
 	}
 
-	fmt.Println("You scored:", score, "/", totalQuestions)
+	return problems
 }

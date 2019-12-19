aa9a3416efd9910ddf605c1c06b35f17c9b4d781
Shan Gardezi <shangardezi@gmail.com>
2019-12-19 13:27:52 +0000

    Clean up error handling

--- ex1-quiz/part1.go
+++ ex1-quiz/part1.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
-	"log"
 	"os"
 	"strings"
 )
@@ -16,25 +15,31 @@ func main() {
 	csvFilename := flag.String("filename", "problems.csv", "The CSV file's name in the format of 'question,answer'")
 	flag.Parse()
 
-	fmt.Println("Press enter to start the quiz")
-	buf := bufio.NewReader(os.Stdin)
-	_, err := buf.ReadBytes('\n')
+	data, err := ioutil.ReadFile(*csvFilename)
 
 	if err != nil {
-		fmt.Println(err)
-	} else {
-		quiz(csvFilename)
-	}
-}
-
-func quiz(fileName *string) {
-	data, err := ioutil.ReadFile(*fileName)
-
-	if err != nil {
-		panic(err)
+		error(fmt.Sprintf("Unable to open file named: %s", *csvFilename))
 	}
 
 	r := csv.NewReader(strings.NewReader(string(data)))
+	fmt.Println("Press enter to start the quiz")
+
+	buf := bufio.NewReader(os.Stdin)
+	_, err2 := buf.ReadBytes('\n')
+
+	if err2 != nil {
+        error("Unable to start quiz")
+	} else {
+		quiz(r)
+	}
+}
+
+func error(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
+}
+
+func quiz(r *csv.Reader) {
 	reader := bufio.NewReader(os.Stdin)
 	score := 0
 	totalQuestions := 0
@@ -45,7 +50,7 @@ func quiz(fileName *string) {
 			break
 		}
 		if err != nil {
-			log.Fatal(err)
+            error("Error reading file data")
 		}
 
 		fmt.Println(record[0])
